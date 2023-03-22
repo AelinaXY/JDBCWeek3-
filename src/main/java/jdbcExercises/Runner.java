b1ca4168fad0b6fc30af9872e5b0c8d910fbd2ad
AelinaXY <Lauren@2infinity.net>
2023-03-22 11:27:58 +0000

    Add test connection

--- src/main/java/jdbcExercises/Runner.java
+++ src/main/java/jdbcExercises/Runner.java
@@ -1,11 +1,23 @@
 package jdbcExercises;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Runner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test");
+		String url = "jdbc:mysql://localhost:3306/petexercise";
+		String username = "root";
+		String password = "pass";
 
+		System.out.println("Connecting database...");
+
+		try (Connection connection = DriverManager.getConnection(url, username, password)) {
+			System.out.println("Database connected!");
+		} catch (SQLException e) {
+			throw new IllegalStateException("Cannot connect the database!", e);
+		}
 	}
-
 }
